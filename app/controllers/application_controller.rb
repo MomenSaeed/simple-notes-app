a60a7754eeac2ab119d272f309dc8a81a4b0c27b
moamen saeed <momenfci@gmail.com>
2020-08-14 13:35:26 +0200

    add users and install devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(resource)
+    sign_in_url = new_user_session_url
+    if request.referer == sign_in_url
+      super
+    else
+      stored_location_for(resource) || request.referer || vendors_path
+    end
+  end
 end
